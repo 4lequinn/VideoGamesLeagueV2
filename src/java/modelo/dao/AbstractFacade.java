97061863f9c632128305f5b7818099dd1a8e9433
4lequinn <jorgealequinn@gmail.com>
2021-10-07 21:59:19 -0300

    Cambio de Nombres de los métodos DAO

--- src/java/modelo/dao/AbstractFacade.java
+++ src/java/modelo/dao/AbstractFacade.java
@@ -22,29 +22,41 @@ public abstract class AbstractFacade<T> {
 
     protected abstract EntityManager getEntityManager();
 
-    public void create(T entity) {
+    // Método para agregar una entidad
+    // Replace create - agregar
+    public void agregar(T entity) {
         getEntityManager().persist(entity);
     }
-
-    public void edit(T entity) {
+    
+    // Método para modificar una entidad
+    // Replace edit - modificar
+    public void modificar(T entity) {
         getEntityManager().merge(entity);
     }
 
-    public void remove(T entity) {
+    // Método para eliminar una entidad
+    // Replace remove - eliminar
+    public void eliminar(T entity) {
         getEntityManager().remove(getEntityManager().merge(entity));
     }
-
+    
+    // Método para buscar una entidad por ID
+    // Replace find - buscar
     public T find(Object id) {
         return getEntityManager().find(entityClass, id);
     }
 
-    public List<T> findAll() {
+    // Método para listar las entidades
+    // Replace findAll - listar
+    public List<T> listar() {
         javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
         cq.select(cq.from(entityClass));
         return getEntityManager().createQuery(cq).getResultList();
     }
-
-    public List<T> findRange(int[] range) {
+    
+    // Método para filtar por un rango una entidad
+    // Replace findRange - filtarRango
+    public List<T> filtrarRango(int[] range) {
         javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
         cq.select(cq.from(entityClass));
         javax.persistence.Query q = getEntityManager().createQuery(cq);
@@ -52,8 +64,10 @@ public abstract class AbstractFacade<T> {
         q.setFirstResult(range[0]);
         return q.getResultList();
     }
-
-    public int count() {
+    
+    // Método para contar las entidades
+    // Replace count - cantidad
+    public int cantidad() {
         javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
         javax.persistence.criteria.Root<T> rt = cq.from(entityClass);
         cq.select(getEntityManager().getCriteriaBuilder().count(rt));
